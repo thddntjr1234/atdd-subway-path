0d63d37a0f9b4f6fee3c85af8e0517cd91852906
thddntjr1234 <thddntjr112233@gmail.com>
2024-07-21 17:21:48 +0900

    1단계 - 구간 추가 단위 테스트 작성

--- src/test/java/nextstep/subway/unit/SectionTest.java
+++ src/test/java/nextstep/subway/unit/SectionTest.java
@@ -10,81 +10,109 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.NoSuchElementException;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("지하철 노선 구간 단위테스트")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class SectionTest {
-    private Line line;
-    private Station upStation;
-    private Station downStation;
+    private Line 인천1호선;
+    private Station 계양역;
+    private Station 국제업무지구역;
+    private Station 인천터미널역;
+    private Station 송도달빛축제공원역;
+    private Station 신검암중앙역;
 
     @BeforeEach
     void setup() {
         //given
-        upStation = new Station(1L, "계양역");
-        downStation = new Station(2L, "국제업무지구역");
-        Section section = new Section(upStation, downStation, 15);
-        line = new Line("인천1호선", "bg-blue-400", section);
+        계양역 = new Station(1L, "계양역");
+        국제업무지구역 = new Station(2L, "국제업무지구역");
+        Section section = new Section(계양역, 국제업무지구역, 15);
+        인천1호선 = new Line("인천1호선", "bg-blue-400", section);
+
+        인천터미널역 = new Station(3L, "인천터미널역");
+        신검암중앙역 = new Station(4L, "신검암중앙역");
+        송도달빛축제공원역 = new Station(5L, "송도달빛축제공원역");
     }
 
-    @DisplayName("이미 해당 노선에 등록된 역은 새로운 구간의 하행역으로 추가할 수 없다.")
+
+    @DisplayName("새 구간을 노선의 끝에 추가한다.")
     @Test
-    void downStationFailure() {
+    void successAddToEnd() {
         //when
-        Station newUpStation = upStation;
-        Station newDownStation = new Station("계양역");
-        Section newSection = new Section(newUpStation, newDownStation, 3);
+        Section 신구간 = new Section(국제업무지구역, 송도달빛축제공원역, 3);
+
+        인천1호선.addSection(신구간);
 
         //then
-        assertThatThrownBy(() -> line.addSection(newSection)).isInstanceOf(IllegalArgumentException.class);
+        assertThat(인천1호선.getSections()).last().isEqualTo(신구간);
     }
 
-    @DisplayName("새로운 구간의 상행역은 해당 노선에 등록되어있는 하행 종점역이어야 한다.")
+    @DisplayName("새 구간을 노선의 처음에 추가한다.")
     @Test
-    void upStationFailure() {
+    void successAddToFirst() {
         //when
-        Station newUpStation = upStation;
-        Station newDownStation = new Station(3L, "송도달빛축제공원역");
-        Section newSection = new Section(newUpStation, newDownStation, 3);
+        Section 신구간 = new Section(신검암중앙역, 계양역, 3);
+
+        인천1호선.addSection(신구간);
 
         //then
-        assertThatThrownBy(() -> line.addSection(newSection)).isInstanceOf(IllegalArgumentException.class);
+        assertThat(인천1호선.getSections()).first().isEqualTo(신구간);
     }
 
-    @DisplayName("정상적인 구간을 추가했을 때 새로운 구간이 노선에 추가된다.")
+    @DisplayName("상행역이 동일한 새 구간을 노선의 가운데에 추가한다.")
     @Test
-    void successAdd() {
+    void successAddToMiddleUpStaionCase() {
         //when
-        Station newUpStation = downStation;
-        Station newDownStation = new Station(3L, "송도달빛축제공원역");
-        Section newSection = new Section(newUpStation, newDownStation, 3);
+        Section 신구간 = new Section(계양역, 인천터미널역, 3);
 
-        line.addSection(newSection);
+        인천1호선.addSection(신구간);
 
         //then
-        assertThat(line.getSections()).containsAnyOf(newSection);
+        assertThat(인천1호선.getSections()).containsAnyOf(신구간);
     }
+
+    @DisplayName("하행역이 동일한 새 구간을 노선의 가운데에 추가한다.")
+    @Test
+    void successAddToMiddleDownStationCase() {
+        //when
+        Section 신구간 = new Section(계양역, 인천터미널역, 3);
+
+        인천1호선.addSection(신구간);
+
+        //then
+        assertThat(인천1호선.getSections()).containsAnyOf(신구간);
+    }
+
+    @DisplayName("노선 가운데에 추가되는 구간의 거리가 노선의 거리보다 짧지 않으면 구간 추가에 실패한다.")
+    @Test
+    void failAddWhenInvalidDistance() {
+        //when
+        Section 신구간 = new Section(계양역, 인천터미널역, 16);
+
+        //then
+        assertThatThrownBy(() -> 인천1호선.addSection(신구간)).isInstanceOf(IllegalArgumentException.class);
+    }
+
 
     @DisplayName("구간이 1개인 경우 역을 삭제할 수 없다.")
     @Test
     void deleteFailure() {
         //given
-        Station deleteTarget = downStation;
-        assertThatThrownBy(() -> line.deleteSection(deleteTarget)).isInstanceOf(NoSuchElementException.class);
+        Station deleteTarget = 국제업무지구역;
+        assertThatThrownBy(() -> 인천1호선.deleteSection(deleteTarget)).isInstanceOf(NoSuchElementException.class);
     }
 
     @DisplayName("정상 구간을 제거한다.")
     @Test
     void successDelete() {
         //when
-        Station newUpStation = downStation;
+        Station newUpStation = 국제업무지구역;
         Station newDownStation = new Station(3L, "송도달빛축제공원역");
         Section newSection = new Section(newUpStation, newDownStation, 3);
-        line.addSection(newSection);
+        인천1호선.addSection(newSection);
 
-        line.deleteSection(newDownStation);
-        assertThat(line.getSections().get(0)).isEqualTo(new Section(upStation, downStation, 15));
+        인천1호선.deleteSection(newDownStation);
+        assertThat(인천1호선.getSections().get(0)).isEqualTo(new Section(계양역, 국제업무지구역, 15));
     }
 }
