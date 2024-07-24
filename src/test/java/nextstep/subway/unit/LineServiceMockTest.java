32990b95a04bb967308a38d4cb092b8695836543
Wooseok Song <thddntjr112233@gmail.com>
2024-07-24 13:29:45 +0900

    1단계 - 구간 추가 요구사항 반영 (#835)
    * 이전 미션에서 사용한 코드 이전
    
    * 실습 - 구간 단위 테스트 작성
    
    * 실습 - 구간 서비스 단위 테스트
    
    - Mock, 실제 객체를 사용한 구간 서비스 단위 테스트
    
    * 코드 리뷰때 질문할 것들
    
    * 1단계 - 테스트 격리 방식을 sql -> 테이블 탐색 후 삭제 방식으로 변경
    
    * 1단계 - 구간 추가 인수 테스트 작성
    
    * 1단계 - 구간 추가 단위 테스트 작성
    
    * 1단계 - 구간 추가 기능 구현
    
    * 1단계 - 중간역 추가 기능 중 하행역 동일 기준 프로세스를 마친 뒤 return 수행
    
    * 1단계 - 구간 추가 서비스 테스트 작성
    
    * 1단계 - 구간 추가 Mock 서비스 테스트 작성
    
    * 1단계 - 리뷰 질문내용 기록 삭제
    
    * 1단계 - 구간 단위테스트 누락된 부분 수정
    
    * 1단계 - 구간 추가 단위기능 로직 오류 수정
    
    - 구간 끝 부분 추가 시 하행종점역과 신규 구간 상행종점역 일치 여부 검사구문 수정
    
    * 1단계 - 단위 테스트에 있던 SpringBootTest 제거
    
    * 1단계 - List<Section> sections를 일급 컬렉션으로 변경
    
    * 1단계 - size == 0 로직을 isEmpty()로 변경
    
    * 1단계 - sections.get()을 직접 호출하지 않고 객체의 메서드를 호출하도록 변경
    
    * 1단계 - 불필요한 조건문 제거
    
    * 1단계 - 주석 수정
    
    * 1단계 - 메서드 오탈자 수정
    
    * 1단계 - ResponseEntity 반환타입 명시적 표현
    
    * 1단계 - respository.finById 래핑 메서드 추가
    
    * 1단계 - 수정 로직에서 더티 체킹 외에도 save()를 수행하던 로직을 제거
    
    * 1단계 - 별도 DB 클리너 사용으로 사용하지 않는 sql 제거
    
    * 1단계 - 역 생성시 반환값의 id를 활용하여 삭제 요청하도록 변경
    
    * 1단계 - 동일구간 추가에 대한 단위 테스트 추가
    
    * 1단계 - 구간 추가 이전 작성했던 삭제 기능이 인스턴스 변수를 사용하도록 변경
    
    * 1단계 - 불필요하게 중복되는 findLineById 수정, 하드 코딩된 id값을 선언한 역 변수에서 가져오도록 수정

--- src/test/java/nextstep/subway/unit/LineServiceMockTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceMockTest.java
@@ -1,9 +1,26 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.domain.line.Line;
+import nextstep.subway.domain.line.LineRepository;
+import nextstep.subway.domain.line.LineService;
+import nextstep.subway.domain.section.Section;
+import nextstep.subway.domain.section.dto.SectionCreateRequest;
+import nextstep.subway.domain.station.Station;
+import nextstep.subway.domain.station.StationService;
+import nextstep.subway.domain.station.dto.StationResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class LineServiceMockTest {
@@ -11,16 +28,130 @@ public class LineServiceMockTest {
     private LineRepository lineRepository;
     @Mock
     private StationService stationService;
+    private LineService lineService;
 
+    private Line 인천1호선;
+    private Section 인천1호선_구간;
+    private Station 계양역;
+    private Station 국제업무지구역;
+    private Station 인천터미널역;
+    private Station 송도달빛축제공원역;
+    private Station 신검암중앙역;
+
+    @BeforeEach
+    public void setUp() {
+        lineService = new LineService(stationService, lineRepository);
+        계양역 = new Station(1L, "계양역");
+        국제업무지구역 = new Station(2L, "국제업무지구역");
+        송도달빛축제공원역 = new Station(3L, "송도달빛축제공원역");
+        신검암중앙역 = new Station(4L, "신검암중앙역");
+        인천터미널역 = new Station(5L, "인천터미널역");
+        인천1호선_구간 = new Section(계양역, 국제업무지구역, 15);
+        인천1호선 = new Line("인천1호선", "bg-blue-400", 인천1호선_구간);
+    }
+
+    @DisplayName("새로운 구간을 노선의 끝에 추가한다.")
     @Test
-    void addSection() {
+    void addSectionToLast() {
         // given
         // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        var 인천1호선_신구간 = new Section(국제업무지구역, 송도달빛축제공원역, 3);
+
+        when(lineRepository.findById(인천1호선.getId())).thenReturn(Optional.of(인천1호선));
+        when(stationService.findByStationId(국제업무지구역.getId())).thenReturn(국제업무지구역);
+        when(stationService.findByStationId(송도달빛축제공원역.getId())).thenReturn(송도달빛축제공원역);
+
 
         // when
         // lineService.addSection 호출
+        var 인천1호선_신구간_생성_요청 = SectionCreateRequest.of(인천1호선_신구간);
+        lineService.addSection(인천1호선.getId(), 인천1호선_신구간_생성_요청);
 
         // then
         // lineService.findLineById 메서드를 통해 검증
+        var stations = lineService.findLineById(인천1호선.getId()).getStations();
+        assertThat(stations).isEqualTo(3);
+        assertThat(stations.stream()
+                .map(StationResponse::getName)
+                .distinct()
+                .collect(Collectors.toList())).containsExactly("계양역", "국제업무지구역", "송도달빛축제공원역");
+    }
+
+    @DisplayName("새로운 구간을 노선의 처음에 추가한다.")
+    @Test
+    void addSectionToFirst() {
+        // given
+        // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        var 인천1호선_신구간 = new Section(신검암중앙역, 계양역, 6);
+
+        when(lineRepository.findById(인천1호선.getId())).thenReturn(Optional.of(인천1호선));
+        when(stationService.findByStationId(계양역.getId())).thenReturn(계양역);
+        when(stationService.findByStationId(신검암중앙역.getId())).thenReturn(신검암중앙역);
+
+        // when
+        // lineService.addSection 호출
+        var 인천1호선_신구간_생성_요청 = SectionCreateRequest.of(인천1호선_신구간);
+        lineService.addSection(인천1호선.getId(), 인천1호선_신구간_생성_요청);
+
+        // then
+        // lineService.findLineById 메서드를 통해 검증
+        var stations = lineService.findLineById(인천1호선.getId()).getStations();
+        assertThat(stations).isEqualTo(3);
+        assertThat(stations.stream()
+                .map(StationResponse::getName)
+                .distinct()
+                .collect(Collectors.toList())).containsExactly("신검암중앙역", "계양역", "국제업무지구역");
+    }
+
+    @DisplayName("기준역이 하행종점역인 새로운 구간을 노선의 가운데에 추가한다.")
+    @Test
+    void addSectionToMiddleBasedOnDownStation() {
+        // given
+        // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        var 인천1호선_신구간 = new Section(인천터미널역, 국제업무지구역, 3);
+
+        when(lineRepository.findById(인천1호선.getId())).thenReturn(Optional.of(인천1호선));
+        when(stationService.findByStationId(국제업무지구역.getId())).thenReturn(국제업무지구역);
+        when(stationService.findByStationId(인천터미널역.getId())).thenReturn(인천터미널역);
+
+        // when
+        // lineService.addSection 호출
+        var 인천1호선_신구간_생성_요청 = SectionCreateRequest.of(인천1호선_신구간);
+        lineService.addSection(인천1호선.getId(), 인천1호선_신구간_생성_요청);
+
+        // then
+        // lineService.findLineById 메서드를 통해 검증
+        var stations = lineService.findLineById(인천1호선.getId()).getStations();
+        assertThat(stations.size()).isEqualTo(3);
+        assertThat(stations.stream()
+                .map(StationResponse::getName)
+                .distinct()
+                .collect(Collectors.toList())).containsExactly("계양역", "인천터미널역", "국제업무지구역");
+    }
+
+    @DisplayName("기준역이 상행종점역인 새로운 구간을 노선의 가운데에 추가한다.")
+    @Test
+    void addSectionToMiddleBasedOnUpStation() {
+        // given
+        // lineRepository, stationService stub 설정을 통해 초기값 셋팅
+        var 인천1호선_신구간 = new Section(계양역, 인천터미널역, 5);
+
+        when(lineRepository.findById(인천1호선.getId())).thenReturn(Optional.of(인천1호선));
+        when(stationService.findByStationId(계양역.getId())).thenReturn(계양역);
+        when(stationService.findByStationId(인천터미널역.getId())).thenReturn(인천터미널역);
+
+        // when
+        // lineService.addSection 호출
+        var 인천1호선_신구간_생성_요청 = SectionCreateRequest.of(인천1호선_신구간);
+        lineService.addSection(인천1호선.getId(), 인천1호선_신구간_생성_요청);
+
+        // then
+        // lineService.findLineById 메서드를 통해 검증
+        var stations = lineService.findLineById(인천1호선.getId()).getStations();
+        assertThat(stations).isEqualTo(3);
+        assertThat(stations.stream()
+                .map(StationResponse::getName)
+                .distinct()
+                .collect(Collectors.toList())).containsExactly("계양역", "인천터미널역", "국제업무지구역");
     }
 }
--- src/test/java/nextstep/subway/unit/LineServiceTest.java
+++ src/test/java/nextstep/subway/unit/LineServiceTest.java
@@ -1,9 +1,24 @@
 package nextstep.subway.unit;
 
+import nextstep.subway.domain.line.Line;
+import nextstep.subway.domain.line.LineRepository;
+import nextstep.subway.domain.line.LineService;
+import nextstep.subway.domain.section.Section;
+import nextstep.subway.domain.section.dto.SectionCreateRequest;
+import nextstep.subway.domain.station.Station;
+import nextstep.subway.domain.station.StationRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -16,15 +31,121 @@ public class LineServiceTest {
     @Autowired
     private LineService lineService;
 
+    private Line 인천1호선;
+    private Section 인천1호선_구간;
+    private Station 계양역;
+    private Station 국제업무지구역;
+    private Station 인천터미널역;
+    private Station 송도달빛축제공원역;
+    private Station 신검암중앙역;
+
+    @BeforeEach
+    public void setUp() {
+        계양역 = new Station("계양역");
+        국제업무지구역 = new Station("국제업무지구역");
+        인천터미널역 = new Station("인천터미널역");
+        신검암중앙역 = new Station("신검암중앙역");
+        송도달빛축제공원역 = new Station("송도달빛축제공원역");
+
+        Arrays.asList(계양역, 국제업무지구역, 송도달빛축제공원역, 신검암중앙역, 인천터미널역)
+                .forEach(station -> stationRepository.save(station));
+
+        // 노선 저장
+        인천1호선_구간 = new Section(계양역, 국제업무지구역, 15);
+        인천1호선 = new Line("인천1호선", "bg-blue-400", 인천1호선_구간);
+        인천1호선 = lineRepository.save(인천1호선);
+    }
+
+    @DisplayName("새 구간을 노선의 끝에 추가한다.")
     @Test
-    void addSection() {
-        // given
-        // stationRepository와 lineRepository를 활용하여 초기값 셋팅
+    void addSectionToLast() {
+        //given
+        // 새 구간 request dto 생성
+        var 인천1호선_신구간 = new Section(국제업무지구역, 송도달빛축제공원역, 3);
+        var 인천1호선_신구간_생성_요청 = SectionCreateRequest.of(인천1호선_신구간);
 
         // when
         // lineService.addSection 호출
+        lineService.addSection(인천1호선.getId(), 인천1호선_신구간_생성_요청);
 
         // then
         // line.getSections 메서드를 통해 검증
+        Line line = lineRepository.findById(인천1호선.getId()).get();
+        assertThat(line.getSections().size()).isEqualTo(2);
+        assertThat(line.getSections().stream()
+                .flatMap(section -> Stream.of(section.getUpwardStation().getName(), section.getDownwardStation().getName()))
+                .distinct()
+                .collect(Collectors.toList()))
+                .containsExactly("계양역", "국제업무지구역", "송도달빛축제공원역");
+
+    }
+
+    @DisplayName("새 구간을 노선의 처음에 추가한다.")
+    @Test
+    void addSectionToFirst() {
+        //given
+        // 새 구간 request dto 생성
+        var 인천1호선_신구간 = new Section(신검암중앙역, 계양역, 3);
+        var 인천1호선_신구간_생성_요청 = SectionCreateRequest.of(인천1호선_신구간);
+
+        // when
+        // lineService.addSection 호출
+        lineService.addSection(인천1호선.getId(), 인천1호선_신구간_생성_요청);
+
+        // then
+        // line.getSections 메서드를 통해 검증
+        Line line = lineRepository.findById(인천1호선.getId()).get();
+        assertThat(line.getSections().size()).isEqualTo(2);
+        assertThat(line.getSections().stream()
+                .flatMap(section -> Stream.of(section.getUpwardStation().getName(), section.getDownwardStation().getName()))
+                .distinct()
+                .collect(Collectors.toList()))
+                .containsExactly("신검암중앙역", "계양역", "국제업무지구역");
+    }
+
+    @DisplayName("기준역이 상행종점역인 새 구간을 노선의 가운데에 추가한다.")
+    @Test
+    void addSectionToMiddleBasedOnUpStation() {
+        //given
+        // 새 구간 request dto 생성
+        var 인천1호선_신구간 = new Section(계양역, 인천터미널역, 6);
+        var 인천1호선_신구간_생성_요청 = SectionCreateRequest.of(인천1호선_신구간);
+
+        // when
+        // lineService.addSection 호출
+        lineService.addSection(인천1호선.getId(), 인천1호선_신구간_생성_요청);
+
+        // then
+        // line.getSections 메서드를 통해 검증
+        Line line = lineRepository.findById(인천1호선.getId()).get();
+        assertThat(line.getSections().size()).isEqualTo(2);
+        assertThat(line.getSections().stream()
+                .flatMap(section -> Stream.of(section.getUpwardStation().getName(), section.getDownwardStation().getName()))
+                .distinct()
+                .collect(Collectors.toList()))
+                .containsExactly("계양역", "인천터미널역", "국제업무지구역");
+    }
+
+    @DisplayName("기준역이 하행종점역인 새 구간을 노선의 가운데에 추가한다.")
+    @Test
+    void addSectionToMiddleBasedOnDownStation() {
+        //given
+        // 새 구간 request dto 생성
+        var 인천1호선_신구간 = new Section(인천터미널역, 국제업무지구역, 6);
+        var 인천1호선_신구간_생성_요청 = SectionCreateRequest.of(인천1호선_신구간);
+
+        // when
+        // lineService.addSection 호출
+        lineService.addSection(인천1호선.getId(), 인천1호선_신구간_생성_요청);
+
+        // then
+        // line.getSections 메서드를 통해 검증
+        Line line = lineRepository.findById(인천1호선.getId()).get();
+        assertThat(line.getSections().size()).isEqualTo(2);
+        assertThat(line.getSections().stream()
+                .flatMap(section -> Stream.of(section.getUpwardStation().getName(), section.getDownwardStation().getName()))
+                .distinct()
+                .collect(Collectors.toList()))
+                .containsExactly("계양역", "인천터미널역", "국제업무지구역");
     }
 }
